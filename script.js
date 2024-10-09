3dba4ccaf50fa70e3a8a3df05fa64df5ee9612b9
Andrew <andrew-jung@hotmail.com>
2024-10-08 22:19:42 -0700

    Add functioning UI, next need to style.

--- script.js
+++ script.js
@@ -8,23 +8,34 @@ function Gameboard(){
 
   const boardContainer = document.createElement("div");
   boardContainer.classList.add("board-container");
+  document.body.appendChild(boardContainer);
 
   for (let i = 0; i<row ; i++){
     board[i] = [];
     for (let j=0 ; j<column ; j++){
       board[i][j]=0;
+
       const cellContainer = document.createElement("div");
+      cellContainer.setAttribute("row-data", `${i}`);
+      cellContainer.setAttribute("col-data", `${j}`);
       cellContainer.classList.add("cell-container");
-      cellContainer.setAttribute("id", `cell${i}-${j}`);
+      cellContainer.setAttribute("id", `${i}-${j}`);
+      // cellContainer.addEventListener("click", ()=>{
+      //   // const mark = createElement("div");
+      //   cellContainer.textContent = `${game.getActivePlayer().marker}`;
+      //   game.switchPlayer();
+        
+      // });
+
+
+      boardContainer.appendChild(cellContainer);
 
 
     };
   };
   console.log(board);
 
-
-  
-
+  // function to mark the spot specified by player //
   const markSpot = (rowIndex, colIndex, activePlayer) => {
     if (board[rowIndex][colIndex] === 0){
       board[rowIndex][colIndex] = activePlayer.marker;
@@ -36,9 +47,7 @@ function Gameboard(){
     }   
   };
 
-
   // function to get the value of a cell //
-
   const cellValue = (rowIndex, colIndex) =>  board[rowIndex][colIndex];
 
   // return of the factory function Gameboard() //
@@ -90,8 +99,6 @@ function Game(){
           winner = player2;
           console.log(`The Winner is ${player2.name}`)
         }
-
-
         break;
       }
       // check column win 
@@ -106,14 +113,12 @@ function Game(){
           winner = player2;
           console.log(`The Winner is ${player2.name}`)
         }
-
         break;
       }
     }
       // check for diagonal win
     if ((gameboard.board[0][0] === gameboard.board[1][1] && gameboard.board[1][1] === gameboard.board[2][2] && gameboard.board[0][0] != 0) || (gameboard.board[0][2] === gameboard.board[1][1] && gameboard.board[1][1] === gameboard.board[2][0] && gameboard.board[0][2] != 0))  {
-    gameOverStatus = true;
-       
+      gameOverStatus = true;
        if (gameOverStatus && gameboard.board[1][1] === player1.marker){
         winner = player1;
         console.log(`The Winner is ${player1.name}`)
@@ -127,12 +132,18 @@ function Game(){
     // check for tie if board is all full with no winner //
     if (!gameboard.board[0].includes(0) && !gameboard.board[1].includes(0) && !gameboard.board[2].includes(0) ){
       console.log("It's a tie!")
+      gameOverStatus = true;
     }
-      
-    if (gameOverStatus){
-      // console.log("We have a winner"); 
-      }
+
+
+    return gameOverStatus;
     };
+
+
+  
+
+ 
+
 
   return {
     activePlayer,
@@ -147,35 +158,73 @@ function Game(){
 
 // Calling functions to "play" and test the game
 
+
+
+// -------------------------Run The Game----------------------- //
 const gameboard = Gameboard();
 const game = Game();
-gameboard.markSpot(0,0, game.getActivePlayer());
-game.checkGameOver();
-game.switchPlayer();
-gameboard.markSpot(0,2,game.getActivePlayer());
-game.checkGameOver();
-game.switchPlayer();
-gameboard.markSpot(0,1, game.getActivePlayer());
-game.checkGameOver();
-game.switchPlayer();
-gameboard.markSpot(1,0, game.getActivePlayer());
-game.checkGameOver();
-game.switchPlayer();
-gameboard.markSpot(1,2, game.getActivePlayer());
-game.checkGameOver();
-game.switchPlayer();
-gameboard.markSpot(1,1, game.getActivePlayer());
-game.checkGameOver();
-game.switchPlayer();
-gameboard.markSpot(2,0, game.getActivePlayer());
-game.checkGameOver();
-game.switchPlayer();
-gameboard.markSpot(2,2, game.getActivePlayer());
-game.checkGameOver();
-game.switchPlayer();
-gameboard.markSpot(2,1, game.getActivePlayer());
-game.checkGameOver();
-console.log(gameboard.cellValue(2,2));
+
+const cellNodeList = document.querySelectorAll(".cell-container");
+cellNodeList.forEach( (node) => {
+  node.addEventListener("click", ()=>{
+
+    const rowData = node.getAttribute("row-data");
+    const colData = node.getAttribute("col-data");
+
+    if (gameboard.cellValue(rowData,colData) === 0){
+
+
+      node.textContent= `${game.getActivePlayer().marker}`;
+      gameboard.markSpot(rowData,colData, game.getActivePlayer());
+      // game.checkGameOver();
+      if (!game.checkGameOver()){
+        game.switchPlayer();
+      } else  if (game.checkGameOver()){
+        alert("Game Over");
+      }
+      
+      
+
+
+
+    }
+
+    else {
+      alert("Invalid Move!");
+
+    }
+
+  })
+}
+);
+
+// gameboard.markSpot(0,0, game.getActivePlayer());
+// game.checkGameOver();
+// game.switchPlayer();
+// gameboard.markSpot(0,2,game.getActivePlayer());
+// game.checkGameOver();
+// game.switchPlayer();
+// gameboard.markSpot(0,1, game.getActivePlayer());
+// game.checkGameOver();
+// game.switchPlayer();
+// gameboard.markSpot(1,0, game.getActivePlayer());
+// game.checkGameOver();
+// game.switchPlayer();
+// gameboard.markSpot(1,2, game.getActivePlayer());
+// game.checkGameOver();
+// game.switchPlayer();
+// gameboard.markSpot(1,1, game.getActivePlayer());
+// game.checkGameOver();
+// game.switchPlayer();
+// gameboard.markSpot(2,0, game.getActivePlayer());
+// game.checkGameOver();
+// game.switchPlayer();
+// gameboard.markSpot(2,2, game.getActivePlayer());
+// game.checkGameOver();
+// game.switchPlayer();
+// gameboard.markSpot(2,1, game.getActivePlayer());
+// game.checkGameOver();
+// console.log(gameboard.cellValue(2,2));
 
 
 
